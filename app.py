a11502e84b929aed7fafa831238a71e568d49fa1
iamsukhmansaini <asing512@my.centennialcollege.ca>
2024-12-12 21:39:19 -0500

    Bind Flask app to PORT environment ve for Render

--- app.py
+++ app.py
@@ -30,6 +30,4 @@ def predict():
         return jsonify({"error": str(e)}), 400
 
 if __name__ == "__main__":
-    # Use the PORT environment variable or default to 5000
-    port = int(os.environ.get("PORT", 5001))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
